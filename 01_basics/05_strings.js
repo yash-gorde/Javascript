142c4ed5dc5a4aa5dbf5ebc65afb084cf8f0463f
yash-gorde <yashgorde@ieee.org>
2025-01-13 21:43:15 +0000

    operations on strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,40 @@
+// const name = "Yash"
+// const repoCount = 21
+
+// console.log(name + repoCount + " value");
+
+// console.log(`hello, my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Yash-Rajendra-Gorde')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+
+// const newStringOne = "         yash     "
+
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+// const url = "https://yash.com/yash%20gorde"
+
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('yash'))
+
+
+console.log(gameName.split('-'));
+
+
