7876d1e2f3aeca32733e0935044c5b85c405d93e
yash-gorde <yashgorde@ieee.org>
2025-01-13 22:12:07 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,32 @@
+// const score = 421
+// console.log(score);
+
+// const balance = new Number(421)
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 121.512121
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++ Maths ++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.floor(4.3));
+
+// console.log(Math.min(1, 2, 3, 4, 5));
+// console.log(Math.max(1, 2, 3, 4, 5));
+
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random() * (max - min + 1)) + min));
