52e952e35bf8db527310eeb2f4d69fc9b980be32
yash-gorde <yashgorde@ieee.org>
2025-01-13 21:04:23 +0000

    datatype summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,34 @@
+// Primitive
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('21')
+// const anotherId = Symbol('21')
+// console.log(id == anotherId);
+
+const bigNumber = 979139274325017597n
+
+
+// Reference (Non Primitive)
+
+// Array, Object, Functions
+
+
+// const heros = ["shaktiman", 'naagraj', 'doga']
+
+// let myObj = {
+//     name: "yash",
+//     age: 21
+// }
+
+const myFunction = function() {
+    console.log("hello, world");
+}
+
+console.log(typeof bigNumber);
