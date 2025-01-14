0e9d72d09d090f2d12a0bff9858275ac84475f92
yash-gorde <yashgorde@ieee.org>
2025-01-14 10:19:02 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,49 @@
+// if 
+// const isUserLoggedIn = true
+// const temperature = 41
+
+// if (temperature < 50) {
+//     console.log("temperature less than 50");
+// } else {
+//     console.log("temperature greater than 50");
+// }
+
+// <, >, <=, >=, !=, ==, ===, !==
+
+const score = 201
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+
+
+
+const balance = 1000
+// if(balance > 500) console.log("test"), console.log("test2"); // not a good way to write code
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900")
+// } else {
+//     console.log("less than 1200");
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+// if(userLoggedIn && debitCard) {
+//     console.log("Allow to buy course");
+// }
+
+if(loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
